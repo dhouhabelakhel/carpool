ef1968cef19115c104810091466ba279664d2964
fares <faresbenromdhank@gmail.com>
2024-11-13 05:12:19 +0100

    add delete in tripOffer

--- backend/Routes/TripOfferRoutes.js
+++ backend/Routes/TripOfferRoutes.js
@@ -1,9 +1,10 @@
-const express=require('express')
-const Router=express.Router();
-const authMiddleware=require('../Middlewares/authMiddleware')
-const tripOfferController=require('../Controllers/TripOfferController')
-Router.get('/',tripOfferController.GetAllTripOffers)
-Router.post('/',tripOfferController.addTripOffer)
-Router.put('/:id',tripOfferController.update)
-
-module.exports=Router
+    const express=require('express')
+    const Router=express.Router();
+    const authMiddleware=require('../Middlewares/authMiddleware')
+    const tripOfferController=require('../Controllers/TripOfferController')
+    Router.get('/',tripOfferController.GetAllTripOffers)
+    Router.post('/',tripOfferController.addTripOffer)
+    Router.put('/:id',tripOfferController.update)
+    Router.delete('/:id', tripOfferController.deleteTripOffer);
+    
+    module.exports=Router
